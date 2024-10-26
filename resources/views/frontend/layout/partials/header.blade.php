bfaa46334f96046982004e124853e12f7689bb03
PrabirGhosh25 <prabirghosh9333@gmail.com>
2024-10-26 21:06:32 +0530

    frontend layout setup done of home and aboutus

---
+++ resources/views/frontend/layout/partials/header.blade.php
@@ -0,0 +1,128 @@
+<header id="main-header" class="header">
+        <div id="top-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-4 logo-box">
+                        <div class="logo">
+                            <img src="{{asset('frontend-asset/images/logo.png')}}" class="img-fluid">
+                        </div>
+                    </div>
+
+
+                    <!-- Section: Navbar Menu -->
+                    <div class="col-lg-8 col-8 align-self-center">
+                        <div class="overlay"></div>
+                        <nav class="menu">
+                            <div class="menu-mobile-header">
+                                <button type="button" class="menu-mobile-arrow"><i
+                                        class="ion ion-ios-arrow-back"></i></button>
+                                <div class="menu-mobile-title"></div>
+                                <button type="button" class="menu-mobile-close"><i
+                                        class="ion ion-ios-close"></i></button>
+                            </div>
+                            <ul class="menu-section">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about-us.html">About Us</a></li>
+
+                                <li class="menu-item-has-children">
+                                    <a href="#">Product <i class="ion ion-ios-arrow-down"></i></a>
+                                    <div class="menu-subs menu-mega menu-column-5">
+                                        <div class="list-item">
+                                            <h4 class="title">Rice & Rice Products</h4>
+                                            <hr>
+                                            <ul>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <!--<li class="menu-btncls"><a href="#">View More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></li>-->
+                                            </ul>
+                                        </div>
+                                        <div class="list-item">
+                                            <h4 class="title">Organic Oil</h4>
+                                            <hr>
+                                            <ul>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <!--<li class="menu-btncls"><a href="#">View More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></li>-->
+                                            </ul>
+                                        </div>
+                                        <div class="list-item">
+                                            <h4 class="title">Organic Dry Fruits</h4>
+                                            <hr>
+                                            <ul>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <!--<li class="menu-btncls"><a href="#">View More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></li>-->
+                                            </ul>
+                                        </div>
+                                        <div class="list-item">
+                                            <h4 class="title">Sugar, Salt & Jaggery</h4>
+                                            <hr>
+                                            <ul>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <li><a href="product.html">Product Name Here</a></li>
+                                                <!--<li class="menu-btncls"><a href="#">View More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></li>-->
+                                            </ul>
+                                        </div>
+
+
+
+
+                                        <div class="list-item">
+                                            <img src="{{asset('frontend-asset/images/menu-pic2.png')}}" class="img-fluid" alt="...">
+                                        </div>
+                                    </div>
+                                </li>
+                                <li><a href="certificate.html">Certificate</a></li>
+                                <li><a href="contact-us.html">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                        <div class="header-item-right">
+                            <button type="button" class="menu-mobile-trigger">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-4 align-self-center mt--1">
+                        <div id="header-1" class="headersearch">
+                            <nav class="header-nav">
+                                <div class="search-button">
+                                    <a href="#" class="search-toggle" data-selector="#header-1"></a>
+                                </div>
+                                <ul class="head-account">
+                                    <li><a href="account.html">Account</a></li>
+                                    <li><a href="cart.html"><i class="fa fa-shopping-bag"
+                                                aria-hidden="true"></i><sup>(0)</sup></a></li>
+                                </ul>
+                                <form action="" class="search-box">
+                                    <input type="text" class="text search-input" placeholder="Type here to search..." />
+                                </form>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
+});
+
+
+Route::get('/about-us', function () {
+    return view('frontend.aboutus');
 });
